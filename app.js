689242a6d88ccca65c8c2fd3db89c9c2bb858367
Juan <sebas981@live.com>
2022-05-02 17:33:11 -0300

    Creacion de POO

--- app.js
+++ app.js
@@ -1,195 +1,55 @@
-//POO
-//usuarios modelos
+//BOM
+//Browser Objet Model
 
-class Usuario{ //el nombre de la clase comienza con mayúscula y en singular.
-  constructor(nombre,username,passwrod,estado){
-    this.nombre = nombre;
-    this.pass = passwrod;
-    this.username = username;
-    this.estado = estado;
-  }
-  presentacion(){
-    console.log('--------Tarjeta de presentacion--------');
-    console.log(`Nombre: ${this.nombre}`);
-    console.log(`Nombre de usuario: ${this.username}`);
-  }
-}
-let usuario1 = new Usuario('Pepito','pepe','1234',true);
-let user2 = new Usuario('Josaefina','jose2022','123ttt');
-//En los prototipos se guardan los metodos
-//Agregar nueva funcion
-Usuario.prototype.saludar=function(){
-  console.log(`Hola soy ${this.nombre}`)
+//window
+//Obtener nuestra posicion
+const obtenerUbicacion=function(){
+  navigator.geolocation.getCurrentPosition(function(posicion){
+    console.log(posicion.coords.latitude, posicion.coords.longitude);
+  })
 }
 
-//Herencia
- class Administrador extends Usuario{
-   constructor(nombre, username, passwrod,estado = true, puesto){
-     super(nombre, username, passwrod, estado);
-     //Aqui traemos las propiedades originales de Usuario, antes de poner this. de usuario
-     this.puesto=puesto;
-   }
-   cambiarEstado(){
-     this.estado!=this.estado //si esta en true lo pondra en false
-   }
-   superPresentacion(){
-     this.presentacion(); //llamamos a la funcion.
-     console.log(`Puesto: ${this.puesto}`);
-   }
- }
+//Location
 
- let admin = new Administrador('Juan','Lara','jdjdjd',true,'Gerente regional');
+Location.reload()//Recargamos la pagina
+Location.href="https://google.com"; //redireccion de pagina
+Location.replace("https://google.com")//reemplazamos la pagina anterir, por la establecida en el link
+Location.replace("https://google.com");
 
- /*
-    Cuenta bancaria
-    1-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
-    - Una propiedad titular.
-    - Una propiedad saldo, teniendo como valor inicial 0.
-    - Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
-    - Un método extraer() que permita retirar la cantidad pasada como parámetro.
-    - Un método informar() que retorne la información del estado de la cuenta. 
-    - Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
-    
-    */
-class Cuenta{
-  constructor(titular,saldo = 0){
-    this.titular = titular;
-    this.saldo = saldo;
-  }
-  ingresar(valor){
-    if(valor >= 0){
-      this.saldo+=valor
-    }else{
-      console.error('El monto debe ser mayor que 0')
-    }
-    this.informar();
-  }
-  extraer(valor){
-    if(valor > this.saldo || valor <0){
-      console.log('Monto inválido o Saldo insuficiente');
-    }else{
-      this.saldo-=valor;
-    }  
-    this.informar();
-  }
-  informar(){
-    console.log('---------Estado de cuenta------');
-    console.log(`Titular: ${this.titular}`);
-    console.log(`Saldo: ${this.saldo}`);
-  }
+//history
+history.go(1)//Esto nos lleva a la página siguiente en el historial, podemos modificar el valor en negativo para retroceder
+
+//Funciones de tiempo
+
+//setTimeout
+//Nos permite ejecutar acciones despues de transcurrido un cierto tiempo.
+const redireccionar=function(){
+  setTimeout(function(){
+    Location.href="www.google.com";
+  },3000);
+}
+//Este recibe una funcion y despues de la , el tiempo en milisegundos
+
+//setInterval
+//Nos permite ejecutar acciones cada cierto tiempo.
+//debemos tener cuidado porque nos puede consumir mucha memoria
+
+function contador(){
+  let num=0;
+  setInterval(function() {
+    console.log(num++);
+  }, 1000);
+}
+function segundero(){
+  repetir=setInterval(function(){
+    console.log(new Date().getSeconds);
+  },1000);
 }
 
-/*
-2- Crea una clase llamada Persona que siga las siguientes condiciones:
-Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
-Los métodos que se debe poder utilizar  son:
-mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
-mostrarDatos: devuelve toda la información del objeto.
-generaDNI(): genera un número aleatorio de 8 cifras.
-*/
-class Cuenta {
-  constructor(titular, saldo = 0) {
-    this.titular = titular;
-    this.saldo = saldo;
-  }
-  ingresar(valor = 0) {
-    if (valor > 0) {
-      this.saldo += valor;
-    } else {
-      console.error("El monto debe ser mayor que 0");
-    }
-    this.informar();
-  }
-  extraer(valor = 0) {
-    if (valor > this.saldo || valor < 0) {
-      console.error("Monto inválido o Saldo insuficiente ");
-    } else {
-      this.saldo -= valor;
-    }
-    this.informar();
-  }
-  informar() {
-    console.log(`-----Estado de Cuenta-----`);
-    console.log(`Titular: ${this.titular}`);
-    console.log(`Saldo: $${this.saldo}`);
-  }
-}
-
-let cuenta001 = new Cuenta("Alejandro Gordillo", 80000);
-let cuenta002 = new Cuenta("Lucia Perez");
-
-/*
-2- Crea una clase llamada Persona que siga las siguientes condiciones:
-Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
-Los métodos que se debe poder utilizar  son:
-mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
-mostrarDatos: devuelve toda la información del objeto.
-generaDNI(): genera un número aleatorio de 8 cifras.
-*/
-
-class Persona {
-  constructor(nombre, edad, sexo, peso, altura, nacimiento, dni) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.dni = dni;
-    this.sexo = sexo;
-    this.peso = peso;
-    this.altura = altura;
-    this.nacimiento = nacimiento;
-  }
-  mostrarGeneracion() {
-    switch (true) {
-      case this.nacimiento >= 1930 && this.nacimiento <= 1948:
-        console.log(
-          `Perteneces a la generación Silent Generation y tu razgo característico es la Austeridad 😐`
-        );
-        break;
-      case this.nacimiento >= 1949 && this.nacimiento <= 1968:
-        console.log(
-          `Perteneces a la generación Baby Boom y tu razgo característico es la Ambicion 🤑`
-        );
-        break;
-      case this.nacimiento >= 1969 && this.nacimiento <= 1980:
-        console.log(
-          "Perteneces a la generación X y tu razco característico es Obsesión por el Éxito 😎"
-        );
-        break;
-      case this.nacimiento >= 1981 && this.nacimiento <= 1993:
-        console.log(
-          "Perteneces a la generación Y y tu razco característico es Frustación 😖"
-        );
-        break;
-      case this.nacimiento >= 1994 && this.nacimiento <= 2010:
-        console.log(
-          "Perteneces a la generación Z y tu razco característico es Irreverencia 😜"
-        );
-        break;
-      default:
-        console.log(
-          "Perteneces a una generación nueva y tu rasgo característico está por descubrirse 🤔"
-        );
-        break;
-    }
-  }
-  mostrarDatos() {
-    console.log("------Datos personales------");
-    console.log(`Nombre: ${this.nombre}`);
-    console.log(`Edad: ${this.edad}`);
-    console.log(`Dni: ${this.dni}`);
-    console.log(`Sexo: ${this.sexo}`);
-    console.log(`Peso: ${this.peso}`);
-    console.log(`Altura: ${this.altura}`);
-    console.log(`Año de nacimiento: ${this.nacimiento}`);
-  }
-  generarDni() {
-    if (!this.dni) {
-      for (let i = 0; i < 8; i++) {
-        let numerito = Math.round(Math.random() * 9);
-        this.dni += String(numerito);
-      }
-      console.log(`El DNI generado es: ${this.dni}`);
-    }
-  }
+//clearInterval
+//Detener intervalo
+const detenerIntervalo = function(){
+  clearInterval(repetir);
 }
 
 
---
+++ fundamentos/11-poo.js
@@ -0,0 +1,233 @@
+//POO
+//usuarios modelos
+
+class Usuario{ //el nombre de la clase comienza con mayúscula y en singular.
+    constructor(nombre,username,passwrod,estado){
+      this.nombre = nombre;
+      this.pass = passwrod;
+      this.username = username;
+      this.estado = estado;
+    }
+    presentacion(){
+      console.log('--------Tarjeta de presentacion--------');
+      console.log(`Nombre: ${this.nombre}`);
+      console.log(`Nombre de usuario: ${this.username}`);
+    }
+  }
+  let usuario1 = new Usuario('Pepito','pepe','1234',true);
+  let user2 = new Usuario('Josaefina','jose2022','123ttt');
+  //En los prototipos se guardan los metodos
+  //Agregar nueva funcion
+  Usuario.prototype.saludar=function(){
+    console.log(`Hola soy ${this.nombre}`)
+  }
+  
+  //Herencia
+   class Administrador extends Usuario{
+     constructor(nombre, username, passwrod,estado = true, puesto){
+       super(nombre, username, passwrod, estado);
+       //Aqui traemos las propiedades originales de Usuario, antes de poner this. de usuario
+       this.puesto=puesto;
+     }
+     cambiarEstado(){
+       this.estado!=this.estado //si esta en true lo pondra en false
+     }
+     superPresentacion(){
+       this.presentacion(); //llamamos a la funcion.
+       console.log(`Puesto: ${this.puesto}`);
+     }
+   }
+  
+   let admin = new Administrador('Juan','Lara','jdjdjd',true,'Gerente regional');
+  
+   /*
+      Cuenta bancaria
+      1-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+      - Una propiedad titular.
+      - Una propiedad saldo, teniendo como valor inicial 0.
+      - Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+      - Un método extraer() que permita retirar la cantidad pasada como parámetro.
+      - Un método informar() que retorne la información del estado de la cuenta. 
+      - Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
+      
+      */
+  class Cuenta{
+    constructor(titular,saldo = 0){
+      this.titular = titular;
+      this.saldo = saldo;
+    }
+    ingresar(valor){
+      if(valor >= 0){
+        this.saldo+=valor
+      }else{
+        console.error('El monto debe ser mayor que 0')
+      }
+      this.informar();
+    }
+    extraer(valor){
+      if(valor > this.saldo || valor <0){
+        console.log('Monto inválido o Saldo insuficiente');
+      }else{
+        this.saldo-=valor;
+      }  
+      this.informar();
+    }
+    informar(){
+      console.log('---------Estado de cuenta------');
+      console.log(`Titular: ${this.titular}`);
+      console.log(`Saldo: ${this.saldo}`);
+    }
+  }
+  
+  /*
+  2- Crea una clase llamada Persona que siga las siguientes condiciones:
+  Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+  Los métodos que se debe poder utilizar  son:
+  mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
+  mostrarDatos: devuelve toda la información del objeto.
+  generaDNI(): genera un número aleatorio de 8 cifras.
+  */
+  class Cuenta {
+    constructor(titular, saldo = 0) {
+      this.titular = titular;
+      this.saldo = saldo;
+    }
+    ingresar(valor = 0) {
+      if (valor > 0) {
+        this.saldo += valor;
+      } else {
+        console.error("El monto debe ser mayor que 0");
+      }
+      this.informar();
+    }
+    extraer(valor = 0) {
+      if (valor > this.saldo || valor < 0) {
+        console.error("Monto inválido o Saldo insuficiente ");
+      } else {
+        this.saldo -= valor;
+      }
+      this.informar();
+    }
+    informar() {
+      console.log(`-----Estado de Cuenta-----`);
+      console.log(`Titular: ${this.titular}`);
+      console.log(`Saldo: $${this.saldo}`);
+    }
+  }
+  
+  let cuenta001 = new Cuenta("Alejandro Gordillo", 80000);
+  let cuenta002 = new Cuenta("Lucia Perez");
+  
+  /*
+  2- Crea una clase llamada Persona que siga las siguientes condiciones:
+  Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+  Los métodos que se debe poder utilizar  son:
+  mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
+  mostrarDatos: devuelve toda la información del objeto.
+  generaDNI(): genera un número aleatorio de 8 cifras.
+  */
+  
+  class Persona {
+    constructor(nombre, edad, sexo, peso, altura, nacimiento, dni) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.dni = dni;
+      this.sexo = sexo;
+      this.peso = peso;
+      this.altura = altura;
+      this.nacimiento = nacimiento;
+    }
+    mostrarGeneracion() {
+      switch (true) {
+        case this.nacimiento >= 1930 && this.nacimiento <= 1948:
+          console.log(
+            `Perteneces a la generación Silent Generation y tu razgo característico es la Austeridad 😐`
+          );
+          break;
+        case this.nacimiento >= 1949 && this.nacimiento <= 1968:
+          console.log(
+            `Perteneces a la generación Baby Boom y tu razgo característico es la Ambicion 🤑`
+          );
+          break;
+        case this.nacimiento >= 1969 && this.nacimiento <= 1980:
+          console.log(
+            "Perteneces a la generación X y tu razco característico es Obsesión por el Éxito 😎"
+          );
+          break;
+        case this.nacimiento >= 1981 && this.nacimiento <= 1993:
+          console.log(
+            "Perteneces a la generación Y y tu razco característico es Frustación 😖"
+          );
+          break;
+        case this.nacimiento >= 1994 && this.nacimiento <= 2010:
+          console.log(
+            "Perteneces a la generación Z y tu razco característico es Irreverencia 😜"
+          );
+          break;
+        default:
+          console.log(
+            "Perteneces a una generación nueva y tu rasgo característico está por descubrirse 🤔"
+          );
+          break;
+      }
+    }
+    mostrarDatos() {
+      console.log("------Datos personales------");
+      console.log(`Nombre: ${this.nombre}`);
+      console.log(`Edad: ${this.edad}`);
+      console.log(`Dni: ${this.dni}`);
+      console.log(`Sexo: ${this.sexo}`);
+      console.log(`Peso: ${this.peso}`);
+      console.log(`Altura: ${this.altura}`);
+      console.log(`Año de nacimiento: ${this.nacimiento}`);
+    }
+    generarDni() {
+      if (!this.dni) {
+        for (let i = 0; i < 8; i++) {
+          let numerito = Math.round(Math.random() * 9);
+          this.dni += String(numerito);
+        }
+        console.log(`El DNI generado es: ${this.dni}`);
+      }
+    }
+  }
+  
+  /*
+  3- Crear una clase Libro que contenga al menos las siguientes propiedades:
+  ISBN
+  Título
+  Autor
+  Número de páginas
+  
+  Crear el método mostrarLibro() para mostrar la información relativa al libro con el siguiente formato:
+  “El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
+  
+  Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
+  Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
+  
+  */
+  class Libro {
+    constructor(isbn,titulo, autor, numpag){
+      this.isbn=isbn;
+      this.titulo=titulo;
+      this.autor=autor;
+      this.numpag=numpag;
+    }
+    mostrarLibro(){
+      console.log(`El libro ${this.titulo} con ISBN ${this.isbn} creado por el autir ${this.autor} tiene ${this.numpag} páginas`);
+    }
+    compararPaginas(Libro){
+      if(this.numpag>Libro.numpag){
+        let cantidadPaginasIf = this.numpag - Libro.numpag;
+        return `El libro ${this.titulo} tiene ${cantidadPaginasIf} que el libro ${Libro.titulo}`
+      }
+      if(this.numpag<Libro.numpag){
+        let cantidadPaginasIf = Libro.numpag -this.numpag
+        return `El libro ${Libro.titulo} tiene ${cantidadPaginasIf} que el libro ${this.titulo}`
+      }
+      return 'Ambos libros tienen la misma cantidad de paginas'
+    }
+    //Libro.numPag, es el objeto que le vamos a mandar para comparar
+  }
+   let libro1 = new Libro('99-566-131-7','Cómo hacer amigos','Dale carnegie',303);
+   let libro2 = new Libro('978-987-1949-58-8','Php desde cero','Users',192);
