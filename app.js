d0bdb5a00c6fa0560db4d7e850a18fbac4c2f394
Juan <sebas981@live.com>
2022-05-04 17:31:10 -0300

    Creacion de documento boom e introduccion de DOM y eventos

--- app.js
+++ app.js
@@ -1,56 +1,85 @@
-//BOM
-//Browser Objet Model
+//DOM
+//DOCUMENT OBJET MODEL
 
-//window
-//Obtener nuestra posicion
-const obtenerUbicacion=function(){
-  navigator.geolocation.getCurrentPosition(function(posicion){
-    console.log(posicion.coords.latitude, posicion.coords.longitude);
-  })
+//Obtener elemento por su etiqueta
+
+let titulo1=document.getElementsByTagName('h1');
+//cuando lo vemos en la consola, lo veremos como una coleccion de h1, y podemos acceder a ellas mediante la posicion del elemento en el array[valor]
+
+//Obtener elemento por su id
+
+let titulo2=document.getElementById('title');
+
+//obtener elemento pot su clase
+
+let titulo3=document.getElementsByClassName('titulo_bonito');
+
+//obtener el primer nodo del codigo html;
+let parrafo1 = document.querySelector('h3');
+
+//obtener todos los nodos del codigo html
+let parrafo2 = document.querySelectorAll('h3');
+
+//obtener el nodo con su id
+let parrafo3 = document.querySelector('#title');
+
+//obtener el nodo a partir del nombre de la clase
+let parrafo4 = document.querySelector('.titulo_bonito');
+
+//NODO: elementos estructurales de html.
+
+//Cambiar el valor a un nodo sin modificar el html
+titulo2.innerText='Bienvenidos al DOM';
+parrafo1.innerText = 'Clase de practica de javascript';
+
+//NOTA: se recomienda utilizar el id para obtener elementos especificos.
+
+//Agregar elementos al DOM
+let nuevoParrafo= document.createElement('p'); //creamos el nodo
+nuevoParrafo.innerText='Esto es un parrafo creado con js';//cargamos el nodo
+document.body.appendChild(nuevoParrafo);
+
+
+let imagen = document.createElement('img');
+imagen.src='https://imagenes.razon.com.mx/files/image_940 47e/uploads/2020/06/e3/5ed7fc823c5e8.jpeg'
+imagen.alt='Imagen bonita';
+imagen.classList="img_dom"; //aplicamos clases a la img
+
+document.body.appendChild(imagen);
+
+//Insertar un nodo antes que otro
+
+//Reemplezar un nodo
+let span=document.createElement('span');
+span.innerText='Esto es una etiqueta span';
+document.body.replaceChild(span,parrafo1);
+
+//quitar un nodo
+document.body.removeChild(imagen);
+
+//Clonar un elemento
+document.body.appendChild(imagen.cloneNode(true));
+
+//------------------------------------------------------------------
+
+let lista =document.getElementById('lista');
+let item =document.createElement('li');
+item.innerText='Hacer la tarea de Gaby';
+let ul = document.querySelector('ul');
+
+ul.appendChild(item)
+
+//-------------EVENTOS------------------------
+function mostrarMensaje(){
+    alert('Esto es un mensaje importante!!');
+}
+function saludar(nombre){
+    console.log(`Hola ${nombre}`);
 }
 
-//Location
-
-Location.reload()//Recargamos la pagina
-Location.href="https://google.com"; //redireccion de pagina
-Location.replace("https://google.com")//reemplazamos la pagina anterir, por la establecida en el link
-Location.replace("https://google.com");
-
-//history
-history.go(1)//Esto nos lleva a la página siguiente en el historial, podemos modificar el valor en negativo para retroceder
-
-//Funciones de tiempo
-
-//setTimeout
-//Nos permite ejecutar acciones despues de transcurrido un cierto tiempo.
-const redireccionar=function(){
-  setTimeout(function(){
-    Location.href="www.google.com";
-  },3000);
+function cambiarColor(){
+    document.querySelector('#campo').classList='fondo';
 }
-//Este recibe una funcion y despues de la , el tiempo en milisegundos
-
-//setInterval
-//Nos permite ejecutar acciones cada cierto tiempo.
-//debemos tener cuidado porque nos puede consumir mucha memoria
-
-function contador(){
-  let num=0;
-  setInterval(function() {
-    console.log(num++);
-  }, 1000);
+function quitarColor(){
+    document.querySelector('#campo').classList='';
 }
-function segundero(){
-  repetir=setInterval(function(){
-    console.log(new Date().getSeconds);
-  },1000);
-}
-
-//clearInterval
-//Detener intervalo
-const detenerIntervalo = function(){
-  clearInterval(repetir);
-}
-
-
-
---
+++ fundamentos/12-bom.js
@@ -0,0 +1,53 @@
+//BOM
+//Browser Objet Model
+
+//window
+//Obtener nuestra posicion
+const obtenerUbicacion=function(){
+    navigator.geolocation.getCurrentPosition(function(posicion){
+      console.log(posicion.coords.latitude, posicion.coords.longitude);
+    })
+  }
+  
+  //Location
+  
+  Location.reload()//Recargamos la pagina
+  Location.href="https://google.com"; //redireccion de pagina
+  Location.replace("https://google.com")//reemplazamos la pagina anterir, por la establecida en el link
+  Location.replace("https://google.com");
+  
+  //history
+  history.go(1)//Esto nos lleva a la página siguiente en el historial, podemos modificar el valor en negativo para retroceder
+  
+  //Funciones de tiempo
+  
+  //setTimeout
+  //Nos permite ejecutar acciones despues de transcurrido un cierto tiempo.
+  const redireccionar=function(){
+    setTimeout(function(){
+      Location.href="www.google.com";
+    },3000);
+  }
+  //Este recibe una funcion y despues de la , el tiempo en milisegundos
+  
+  //setInterval
+  //Nos permite ejecutar acciones cada cierto tiempo.
+  //debemos tener cuidado porque nos puede consumir mucha memoria
+  
+  function contador(){
+    let num=0;
+    setInterval(function() {
+      console.log(num++);
+    }, 1000);
+  }
+  function segundero(){
+    repetir=setInterval(function(){
+      console.log(new Date().getSeconds);
+    },1000);
+  }
+  
+  //clearInterval
+  //Detener intervalo
+  const detenerIntervalo = function(){
+    clearInterval(repetir);
+  }
