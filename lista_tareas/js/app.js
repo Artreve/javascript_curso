bd18da5db04f0519b9f76f6dd1ce1e49282953d1
Juan <sebas981@live.com>
2022-05-15 13:38:55 -0300

    creacion de lista de tareas

--- lista_tareas/js/app.js
+++ lista_tareas/js/app.js
@@ -1,13 +1,25 @@
-let tareas = []
 let counter = document.getElementById('contador');
-let contenedorTarjetas = document.getElementById('contenedor')
+let contenedorTarjetas = document.getElementById('contenedor');
 
+let tareas = JSON.parse (localStorage.getItem("tareas")) || [];
+//JSON.parse tomar la informacion del localStorage y lo tranforma en un array de js, en caso de ser objeto lo tranforma en objeto de js
+
+//Guardar en localStorage
+
+// localStorage.setItem('cositas',JSON.stringify( 'tareas')) //OJO, como la informacion se debe almacenar en formato JSON en el local Storage, debemos mandarlo como esta en la linea de codigo
+
+//NO SE PUEDEN MANDAR FUNCIONES AL LOCAL STORAGE
+
+//Traer informacion de localStorage
+
+// let pruebita = JSON.parse(localStorage.getItem('tareas')) //Como esta en formato JSON, con esto primero convertimos a un arreglo manipulable para js y lo almacenamos en pruebita
 
 const handleSubmit=function(e){
     e.preventDefault();//prevenimos que se realice el refresco de la pantalla al precionar enter
     let tarea = document.getElementById('textTarea');
     tareas.push(tarea.value);//guardamos directamente el valor del elemento en el array
-    console.log(tareas);
+    // console.log(tareas);
+    localStorage.setItem('tareas',JSON.stringify(tareas))
     tarea.value='';//limpiamos el input
     tarea.select();//regresa el foco al campo
     agregarTarjeta(); //CADA VEZ QUE AGREGAMOS ALGO SE RENDERIZA LA TARJETA 
@@ -32,7 +44,15 @@ const agregarTarjeta = function(){
 
 function borrarTarea(index){
     tareas.splice(index,1);
+    localStorage.setItem('tareas', JSON.stringify(tareas));
     agregarTarjeta();
+
 }
 
 document.querySelector('#formulario').addEventListener('submit', handleSubmit);//si lo dejamos con los parentesis se va a llamar a al funcion sin apretar enter, por eso no van
+
+/*Local storage almacena informacion en la direccion en donde estoy, esta permanece hasta que nosotros lo borramos.
+En cambio session Storage almacena informacion y se borra cuando salimos del sitio o cerramos el navegador.
+Dentro del localStorage debemos guardar la informacion en formato JSON*/
+
+agregarTarjeta();
